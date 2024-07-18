1ba09c12b57fdac289c1c8107de798da1936c5f4
Firozrafi <firozrafi123@gmail.com>
2024-07-19 02:26:21 +0530

    Create Celsius.java

---
+++ Celsius.java
@@ -0,0 +1,10 @@
+public class Celsius  
+ {  
+   public static void main (String args[])  
+    { float F , C; //Fahrenheit, Celsiud
+          F = 43;  
+          C= ((F-32)*5)/9;  
+          System.out.println("Temperature in celsius is: "+C);  
+    }
+    
+ }  
